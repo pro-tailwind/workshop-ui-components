774f0755fb31b5c1df6ef8b5b809d8130adfc569
Simon Vrachliotis <simon@simonswiss.com>
2022-08-12 22:45:28 +1000

    Timepicker better mock times

--- stories/time-picker.stories.jsx
+++ stories/time-picker.stories.jsx
@@ -39,84 +39,60 @@ export function ScrollingList() {
   const [selectedDate] = useState(today())
   const availabilities = [
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 8 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 9 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 9 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 10 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 10 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 11 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 11 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 12 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 12 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 13 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 13 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 14 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 14 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 15 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 15 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 16 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 16 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 17 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 17 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 18 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 18 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 19 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 19 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 20 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 20 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 21 }).toString(),
     },
     {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
-    },
-    {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
-    },
-    {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
-    },
-    {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
-    },
-    {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
-    },
-    {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
-    },
-    {
-      startTime: toCalendarDateTime(today()).toString(),
-      endTime: toCalendarDateTime(today()).add({ hours: 1 }).toString(),
+      startTime: toCalendarDateTime(today()).add({ hours: 21 }).toString(),
+      endTime: toCalendarDateTime(today()).add({ hours: 22 }).toString(),
     },
   ]
   return (
